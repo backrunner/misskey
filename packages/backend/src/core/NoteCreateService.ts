2a3e8765ec242170a74b79d4ef7027e5621897b7
BackRunner <dev@backrunner.top>
2024-12-10 01:43:36 +0800

    fix: wrong implementation in noteCreateService

--- packages/backend/src/core/NoteCreateService.ts
+++ packages/backend/src/core/NoteCreateService.ts
@@ -562,7 +562,7 @@ export class NoteCreateService implements OnApplicationShutdown {
 				notify: 'normal',
 			}).then(async followings => {
 				if (note.visibility !== 'specified') {
-					const isPureRenote = this.isRenote(data) && !this.isQuote(data) ? true : false;
+					const isPureRenote = this.isRenote(data) && !this.noteEntityService.isQuote(data) ? true : false;
 					for (const following of followings) {
 						// TODO: ワードミュート考慮
 						let isRenoteMuted = false;
